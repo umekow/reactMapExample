67dd959854c2d19856ca778b7117924791e3c902
umekow <umekolashun@gmail.com>
2019-12-11 14:03:39 -0600

    -imported ReactMapGL into App.js created state called viewport that contains an object to set location and size of map on line 6. -used ReactMapGL component and passed in viewport

--- mapboxexample/src/App.js
+++ mapboxexample/src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import ReactMapGL from 'react-map-gl';
 import './App.css';
 
 function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 32.298760, 
+    longitude: -90.184810, 
+    width: '100vw', 
+    height: '100vh', 
+    zoom: 10
+
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ReactMapGL {...viewport}>
+       markers
+     </ReactMapGL>
     </div>
   );
 }
